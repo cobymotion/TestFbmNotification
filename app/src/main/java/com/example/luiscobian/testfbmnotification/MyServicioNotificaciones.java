5e88e1173e216375c7868473f0128c5cff4a2984
Luis Cobian <coby10@gmail.com>
2017-04-26 12:14:54 -0500

    Ejemplo para notificaciones con FirebaseCloudMessaging

---
+++ app/src/main/java/com/example/luiscobian/testfbmnotification/MyServicioNotificaciones.java
@@ -0,0 +1,50 @@
+package com.example.luiscobian.testfbmnotification;
+
+
+import android.app.NotificationManager;
+import android.content.Context;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by luiscobian on 4/26/17.
+ */
+
+public class MyServicioNotificaciones extends FirebaseMessagingService {
+
+    private static final String LOGTAG = "MyFireStation";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        if (remoteMessage.getNotification() != null) {
+
+            String titulo = remoteMessage.getNotification().getTitle();
+            String texto = remoteMessage.getNotification().getBody();
+
+            Log.d(LOGTAG, "NOTIFICACION RECIBIDA");
+            Log.d(LOGTAG, "Título: " + titulo);
+            Log.d(LOGTAG, "Texto: " + texto);
+
+            //Opcional: mostramos la notificación en la barra de estado
+            showNotification(titulo, texto);
+        }
+    }
+
+    private void showNotification(String title, String text) {
+
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(android.R.drawable.stat_sys_warning)
+                        .setContentTitle(title)
+                        .setContentText(text);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+}
